0ba5959436dc9f3fb2c2e7995884fdbd0fc43017
JamieJiami <75553178+JamieJiami@users.noreply.github.com>
2022-02-13 12:06:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 `use strict`;
 
+var datetime = new Date();
+
+console.log(datetime);
+
+document.getElementById("time").textContent = datetime; //it will print on html page
+
 function refreshTime() {
 
   const timeDisplay = document.getElementById("time");
